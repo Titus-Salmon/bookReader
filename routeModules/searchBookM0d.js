ade27f6a43bb65e10e42c94b15d105ee2cc9883c
tsalm0n <tsalm0n@twc.com>
2020-01-14 03:02:34 -0500

    show fsReadFileResults.fsReadFileResultsLines.indexOf(elmnt) for each elmnt in fsReadFileResults.fsReadFileResultsLines

--- routeModules/searchBookM0d.js
+++ routeModules/searchBookM0d.js
@@ -9,22 +9,31 @@ module.exports = {
 
 		let fileReadPost = req.body['fileReadPost']
 		let searchBookPost = req.body['searchBookPost']
+		let SRArr = []
 
 		fs.readFile(`${process.cwd()}/public/bookFiles/${fileReadPost}`, 'utf8', function (err, data) {
 			if (err) throw err
 
-			const lines = data.split(/\r?\n/)
+			const lines = data.split(/\r?\n/) //splits on carriage return & new line
 
 			if (searchBookPost !== undefined) {
+				// let SRO = {}
 				for (let i = 0; i < lines.length; i++) {
-					let words = lines[i].split(/\s/)
+					// let SRO = {}
+					let words = lines[i].split(/\s/) //splits on spaces
 					for (let j = 0; j < words.length; j++) {
 						if (words[j].toLowerCase() == searchBookPost) {
+							let SRO = {}
+							SRO[`line#`] = i
 							console.log(`search term '${searchBookPost}' found in line#: ${i}`)
+							SRO[`word#`] = j
 							console.log(`within line# ${i}, search term '${searchBookPost}' found in word#: ${j}`)
+							SRArr.push(SRO)
 						}
 					}
 				}
+				console.log(`SRarr==> ${SRArr}`)
+				console.log(`JSON.stringify(SRarr)==> ${JSON.stringify(SRArr)}`)
 			}
 
 			//try creating an object that matches same shape as DOM data, in order to pass targeted search results back to
