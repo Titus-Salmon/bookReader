3b13240f039b6b4134347cb75edd0c267189664e
tsalm0n <tsalm0n@twc.com>
2020-06-02 01:13:06 -0400

    add cycle through results functionality

--- public/javascripts/domModules/textDecM0d.js
+++ public/javascripts/domModules/textDecM0d.js
@@ -73,6 +73,8 @@ for (let y = 0; y < fsResults[0].parentNode.childNodes.length; y++) {
   }
 }
 
+let clickCount = 0
+
 parsedSearchResBtn.addEventListener("click", function () {
   console.log(`saniParsedSearchRes==> ${saniParsedSearchRes}`)
   // for (let i = 0; i < fsResults.length; i++) {
@@ -81,6 +83,20 @@ parsedSearchResBtn.addEventListener("click", function () {
   //     fsResults[i].innerHTML = fsResults[i].innerHTML.replace(`${searchTerm.value}`, `<span style="color: red">${searchTerm.value}</span>`)
   //   }
   // }
+
+  // clickCount++
+
+  let lineZoom = fsResults[0].parentNode.childNodes[JSON.parse(saniParsedSearchResSplit[clickCount])['line#'] + 10] //the # you add at the end here
+  //may vary, depending on the position of the childNodes that the 1st fsResults class element starts (i.e., if you add more DOM elements
+  //before it, it will increase by however many DOM elements you've added. Currently, the value is 10, but if you added 1 additional DOM
+  //element before it, such as a button, you'd have to change the # to 11)
+
+  console.log(`clickCount==> ${clickCount}`)
+
+  lineZoom.scrollIntoView()
+
+  clickCount++
+
 })
 
 console.log(`fsResults[0].parentNode.childNodes[7].innerHTML==> ${fsResults[0].parentNode.childNodes[7].innerHTML}`)
